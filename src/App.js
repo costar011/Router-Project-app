439e10810f28f1bf540f7da17c250506340d3948
YERIM <dnaer456@gmail.com>
2020-09-22 20:01:00 +0900

    div

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ class App extends React.Component {
 
     this.state = {
       timeSecond: 0,
+      Start: false,
+      recode: [],
     };
   }
   render() {
     const { timeSecond } = this.state;
     return (
-      <div className="">
-        <div className=""></div>
+      <div className="time__Box">
+        <div className="time__Box__time">
+          <span>{timeSecond}</span>
+        </div>
+
+        <div className="time___Box__recode"></div>
       </div>
     );
   }
