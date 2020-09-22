c2c852d949925de5377f093ebb85633ff41001a3
YERIM <dnaer456@gmail.com>
2020-09-22 23:19:30 +0900

    first

--- src/App.js
+++ src/App.js
@@ -2,36 +2,11 @@ import React from "react";
 import "./styles/styles.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      timeSecond: 0,
-      Start: false,
-      recode: [],
-    };
-  }
   render() {
-    const { timeSecond, recode } = this.state;
     return (
-      <div className="timeBox">
-        <div className="timeBox__time">
-          <span>{timeSecond}</span>
-        </div>
-        {/*map : 반복 하는 함수*/}
-        <div className="timeBox__record">
-          <ul>
-            {recode.map((code, number) => {
-              return <li key={number}>{code}</li>;
-            })}
-          </ul>
-        </div>
-        <div className="timerBox__btn">
-          <input type="button" value="START"></input>
-          <input type="button" value="STOP"></input>
-          <input type="button" value="RECODE"></input>
-          <input type="button" value="INIT"></input>
-        </div>
+      <div className="app">
+        <div className="app__header">header</div>
+        <div className="app__main">main</div>
       </div>
     );
   }
