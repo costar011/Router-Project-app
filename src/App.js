458e17bf51be8c4d6f5568bebdcbb6de9f3027ea
YERIM <dnaer456@gmail.com>
2020-09-22 18:43:30 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timeSecond: 0,
+    };
+  }
   render() {
-    return <div>Hi</div>;
+    const { timeSecond } = this.state;
+    return (
+      <div className="">
+        <div className=""></div>
+      </div>
+    );
   }
 }
 
